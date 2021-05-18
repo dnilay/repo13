ee4a18ab8c19e02150f686d9aea02af32eded2bc
Nilay <54245048+dnilay@users.noreply.github.com>
2021-05-18 17:27:13 +0530

    Add files via upload

---
+++ spring-mvc-app/src/main/java/org/example/mvc/HomeController.java
@@ -0,0 +1,14 @@
+package org.example.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+
+	@RequestMapping("/")
+	public String showPage() {
+		return "main-menu";
+	}
+}
